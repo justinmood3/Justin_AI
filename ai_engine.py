0132118bf5a0eade7d39a20419e8d78775964a05
justinmood3 <justinmood3@gmail.com>
2026-05-07 16:59:07 +0200

    update1

--- ai_engine.py
+++ ai_engine.py
@@ -1,20 +1,16 @@
 from groq import Groq
-from dotenv import load_dotenv
 import os
-
-load_dotenv()
 
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
-def get_response(user_message):
+def get_response(message):
     try:
         response = client.chat.completions.create(
-            model="llama3-8b-8192",
+            model="llama-3.1-8b-instant",
             messages=[
-                {"role": "user", "content": user_message}
+                {"role": "user", "content": message}
             ]
         )
-
         return response.choices[0].message.content
 
     except Exception as e:
