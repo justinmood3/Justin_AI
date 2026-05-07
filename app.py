d0f1cbb62ba3d284b105ea95e7fc77a782f2d1a3
justinmood3 <justinmood3@gmail.com>
2026-05-07 16:10:51 +0200

    updated

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request
 from ai_engine import get_response
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
+chat_history = []
+
 @app.route("/", methods=["GET", "POST"])
 def home():
+    global chat_history
+
     if request.method == "POST":
-        user_input = request.form.get("message")
-        if not user_input:
-            return "No input provided"
+        user = request.form.get("message")
 
-        reply = get_response(user_input)
-        return reply
+        if not user:
+            return render_template("index.html", chat=chat_history)
 
-    return "AI Chat is running 🚀"
+        ai = get_response(user)
+
+        chat_history.append(("You", user))
+        chat_history.append(("AI", ai))
+
+    return render_template("index.html", chat=chat_history)
 
 if __name__ == "__main__":
     app.run()
