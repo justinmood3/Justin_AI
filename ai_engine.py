bcfbed08a54eeb78baf93b89cfa6b7510be62b88
justinmood3 <justinmood3@gmail.com>
2026-05-07 15:46:11 +0200

    changes

--- ai_engine.py
+++ ai_engine.py
@@ -1,15 +1,12 @@
+from groq import Groq
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-client = OpenAI(
-    api_key=os.getenv("GROQ_API_KEY"),
-    base_url="https://api.groq.com/openai/v1"
-)
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 def get_response(user_message):
-
     try:
         response = client.chat.completions.create(
             model="llama-3.3-70b-versatile",
@@ -17,7 +14,6 @@ def get_response(user_message):
                 {"role": "user", "content": user_message}
             ]
         )
-
         return response.choices[0].message.content
 
     except Exception as e:
